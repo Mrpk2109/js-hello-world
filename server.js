41d769da4caf11057f5a10f63fbdb30c127ae399
Ballphantom <Nokaraza012@gmail.com>
2022-08-28 08:23:20 +0000

    Hello Hello

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) => {
   return res.send('<h1> UP : Computer Engineering XIX </h1>')
 })
 
+app.get('/ballphantom', (req, res) => {
+  return res.send('<h1>ศวัศดีย์ ขวั๊ฟ พรี๊ ฮณุ๋ย</h1>')
+})
+app.get('/ballphantom', (req, res) => {
+  return res.send('<h1>ศวัศดีย์ ขวั๊ฟ พรี๊ ฮณุ๋ย</h1>')
+})
+
+app.get('/ballphantom', (req, res) => {
+  return res.send('<h1>ศวัศดีย์ ขวั๊ฟ พรี๊ ฮณุ๋ย</h1>')
+})
+
+app.get('/ballphantom', (req, res) => {
+  return res.send('<h1>ศวัศดีย์ ขวั๊ฟ พรี๊ ฮณุ๋ย</h1>')
+})
+
+app.get('/ballphantom', (req, res) => {
+  return res.send('<h1>ศวัศดีย์ ขวั๊ฟ พรี๊ ฮณุ๋ย</h1>')
+})
+
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
