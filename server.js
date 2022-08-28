8187210234e3aec89d88910347eede45a54de097
Pwrmsii <heyyou1141@gmail.com>
2022-08-28 08:11:25 +0000

    pluem

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
   return res.send('<h1> UP : Computer Engineering XIX </h1> <h2> UP : New NUI Feture </h2>')
 })
-
+app.get('/pwrmsii', (req, res) => {
+  return res.send('<h1> pwrmsii </h2>')
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
