6596e81f0b4904f008a30769ab83a27363d17766
Nui-backend-intern <oppo9171@gmail.com>
2022-08-28 04:18:45 +0000

    Add more

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1> Hello WORLD </h1>')
+  return res.send('<h1> HELLO WORLD </h1>')
 })
 
 const PORT = process.env.PORT || 3000
