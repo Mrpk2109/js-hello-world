8884679b1cb28138c7d0a1b2ca69b9190182f005
BoomlolZ <boom.rang1234567890@gmail.com>
2022-08-28 07:44:40 +0000

    new

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1> UP : Computer Engineering XIX </h1> <h2> UP : New NUI Feture </h2>')
+  return res.send('<h1> UP : Computer Engineering XIX </h1>')
+})
+app.get('/boom', (req, res) => {
+  return res.send('<h1> UP : BoomlolZ Page</h1>')
 })
 
 const PORT = process.env.PORT || 3000
