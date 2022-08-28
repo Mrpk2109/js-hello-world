47541dbd88b537dbfb280c16a3af72358d719272
Bammu21 <Patcharawarin999@gmail.com>
2022-08-28 08:00:11 +0000

    bam

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
   return res.send('<h1> UP : Computer Engineering XIX </h1> <h2> UP : New NUI Feture </h2>')
 })
+app.get('/', (req, res) => {
+  return res.send('<h1> UP : Computer Engineering XIX </h1> <h2> UP : New NUI Feture </h2>')
+})
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
