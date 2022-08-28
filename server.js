0e1fd304567748846202b5e5aabbe9caad87f6eb
BoomlolZ <boom.rang1234567890@gmail.com>
2022-08-28 07:38:41 +0000

    NUI Feature

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1> UP : Computer Engineering XIX </h1>')
+  return res.send('<h1> UP : Computer Engineering XIX </h1> <h2> UP : New NUI Feture </h2>')
 })
 
 const PORT = process.env.PORT || 3000
